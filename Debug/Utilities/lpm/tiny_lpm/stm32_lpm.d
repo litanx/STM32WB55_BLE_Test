be813acfd69877380219e77fe53bc367d4cb24ee
Diego <diegobailos@gmail.com>
2020-08-12 01:34:00 +0100

    first commit

---
+++ Debug/Utilities/lpm/tiny_lpm/stm32_lpm.d
@@ -0,0 +1,13 @@
+Utilities/lpm/tiny_lpm/stm32_lpm.o: \
+ C:/Users/Tiko/Desktop/Atollic\ Test/BLE_Test2/Utilities/lpm/tiny_lpm/stm32_lpm.c \
+ C:/Users/Tiko/Desktop/Atollic\ Test/BLE_Test2/Utilities/lpm/tiny_lpm/stm32_lpm.h \
+ ../Core/Inc/utilities_conf.h ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h
+
+C:/Users/Tiko/Desktop/Atollic\ Test/BLE_Test2/Utilities/lpm/tiny_lpm/stm32_lpm.h:
+
+../Core/Inc/utilities_conf.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
---
+++ Debug/Utilities/sequencer/stm32_seq.d
@@ -0,0 +1,13 @@
+Utilities/sequencer/stm32_seq.o: \
+ C:/Users/Tiko/Desktop/Atollic\ Test/BLE_Test2/Utilities/sequencer/stm32_seq.c \
+ C:/Users/Tiko/Desktop/Atollic\ Test/BLE_Test2/Utilities/sequencer/stm32_seq.h \
+ ../Core/Inc/utilities_conf.h ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h
+
+C:/Users/Tiko/Desktop/Atollic\ Test/BLE_Test2/Utilities/sequencer/stm32_seq.h:
+
+../Core/Inc/utilities_conf.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
